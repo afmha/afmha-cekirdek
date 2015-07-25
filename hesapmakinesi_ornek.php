35fbd30485f8f0f634a55b54045eaeb4c5db7be8
Afmha <afmha@hotmail.com>
2015-07-25 19:58:20 +0300

    Hesap Makinesi Örneği

---
+++ hesapmakinesi_ornek.php
@@ -0,0 +1,16 @@
+<?php
+
+  require_once("afmhacekirdek.php"); // Afmha Çekirdek yazılımının hangi dizinde olduğunu yazınız.
+  
+  t(5,5);
+  yaz("<br>");
+  c(10,5);
+  yaz("<br>");
+  b(50,5);
+  yaz("<br>");
+  cc(5,5);
+  yaz("<br>");
+  m(59,5);
+  yaz("<br>");
+
+?>
