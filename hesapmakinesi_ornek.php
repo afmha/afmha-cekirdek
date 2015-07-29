a01e1382c42f6f766765b2db633c3b3788088ece
Afmha <afmha@hotmail.com>
2015-07-30 00:15:48 +0300

    Sınıf Sistemi ve Güncelleme
    Sınıf Sistemine uygun düzenlendi.

--- hesapmakinesi_ornek.php
+++ hesapmakinesi_ornek.php
@@ -2,15 +2,15 @@
 
   require_once("afmhacekirdek.php"); // Afmha Çekirdek yazılımının hangi dizinde olduğunu yazınız.
   
-  t(5,5);
+  $hm->t(5,5);
   yaz("<br>");
-  c(10,5);
+  $hm->c(10,5);
   yaz("<br>");
-  b(50,5);
+  $hm->b(50,5);
   yaz("<br>");
-  cc(5,5);
+  $hm->cc(5,5);
   yaz("<br>");
-  m(59,5);
+  $hm->m(59,5);
   yaz("<br>");
 
 ?>
